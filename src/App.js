3c0dbce4dd6ec5cbd1c42d702ef365005afd377d
unknown <mouriuser@MTLP-3551.in.mouritech.net>
2026-07-09 14:23:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Weather from './components/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Weather App - Muraly 2222222</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
